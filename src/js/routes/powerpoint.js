19adf899102b6910520fa571ca1e211436e55271
Julian Camacho <julandro.mza@gmail.com>
2024-11-14 01:40:48 -0500

    Se crea la seccion de PowerPoint

---
+++ src/js/routes/powerpoint.js
@@ -0,0 +1,342 @@
+const routes = {
+  "/pages/powerpoint#informacion": `
+        <section class="section">
+          <h1 class="title has-text-centered">Informacion</h1>
+    
+          <div
+            class="section is-flex is-flex-wrap-wrap is-justify-content-space-evenly"
+          >
+            <div class="section" style="max-width: 500px">
+              <h1 class="title">¿Para qué sirve PowerPoint?</h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Crear presentaciones visuales: </strong>Es ideal para diseñar presentaciones para reuniones, conferencias, clases y eventos.
+                  </li>
+                  <li>
+                    <strong>Visualización de ideas: </strong>Ayuda a presentar conceptos de forma clara y atractiva mediante el uso de diapositivas visualmente organizadas.
+  
+                  </li>
+                  <li>
+                    <strong>Mostrar contenido multimedia: </strong>Permite insertar imágenes, videos, audios, y gráficos interactivos que hacen la presentación más dinámica y entretenida.
+                  </li>
+                  <li>
+                    <strong>Diseño personalizado: </strong>Ofrece herramientas para personalizar el diseño de las diapositivas con plantillas, transiciones, animaciones y efectos.
+                  </li>
+                  <li>
+                    <strong>Impresión de diapositivas: </strong>Permite imprimir presentaciones en formato de diapositivas o en formato de notas, lo que facilita la distribución de los contenidos.
+                  </li>
+                </ul>
+              </div>
+            </div>
+    
+            <div class="section" style="max-width: 500px">
+              <h1 class="title is-size-4">
+                ¿Cuáles son sus características principales?
+              </h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Diapositivas: </strong>Las presentaciones se estructuran en una secuencia de diapositivas que pueden incluir texto, imágenes, gráficos y otros elementos visuales.
+                  </li>
+                  <li>
+                    <strong>Plantillas de diseño: </strong>PowerPoint ofrece una variedad de plantillas prediseñadas para facilitar la creación de presentaciones profesionales.
+                  </li>
+                  <li>
+                    <strong>Animaciones y transiciones: </strong>Puedes añadir efectos visuales a las diapositivas, como animaciones en el texto o elementos gráficos, y transiciones entre diapositivas para que la presentación sea más atractiva.
+                  </li>
+                  <li>
+                    <strong>Herramientas de colaboración: </strong>Permite que varias personas trabajen en la misma presentación al mismo tiempo, y ofrece herramientas de comentarios y sugerencias.
+                  </li>
+                  <li>
+                    <strong>Integración multimedia: </strong>Puedes insertar y editar videos, sonidos, gráficos y enlaces interactivos, lo que hace las presentaciones más dinámicas.
+                  </li>
+                  <li>
+                    <strong>Modo de presentación: </strong>PowerPoint incluye un modo especial de presentación para mostrar las diapositivas en pantalla completa, con la opción de mostrar notas del presentador, controlar el avance de las diapositivas y usar un puntero láser.
+                  </li>
+                  <li>
+                    <strong>Gráficos y diagramas: </strong>Puedes insertar gráficos, tablas y diagramas para ilustrar mejor los datos y conceptos durante la presentación.
+                  </li>
+                </ul>
+              </div>
+            </div>
+    
+            <div class="section" style="max-width: 500px">
+              <h1 class="title">¿Quiénes lo usan?</h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Profesionales: </strong>Para presentar propuestas, informes, proyectos, o informes de ventas a colegas, clientes o directivos.
+                  </li>
+                  <li>
+                    <strong>Estudiantes y educadores: </strong>Para crear y mostrar presentaciones en clase, ya sea para enseñar o presentar trabajos y proyectos.
+                  </li>
+                  <li>
+                    <strong>Empresas: </strong>Para realizar presentaciones en reuniones de negocios, conferencias y seminarios.
+  
+                  </li>
+                  <li>
+                    <strong>Marketers y creativos: </strong>Para mostrar conceptos de diseño, estrategias de marketing o informes de resultados.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        `,
+  "/pages/powerpoint#videos": `<section class="section">
+      <h1 class="title">Videos</h1>
+      <h2 class="subtitle">
+        Estos videos explicativos <strong>no son de nuestra autoria</strong>, al
+        lado esta el nombre del autor y su respectivo link.
+      </h2>
+
+      <br />
+
+      <div class="columns notification">
+        <div class="column content">
+          <h1 class="title">CURSO DE POWERPOINT 2019 - COMPLETO</h1>
+          <p class="has-text-grey-light is-size-5 has-text-justified">
+            Este video es un curso completo de PowerPoint 2019. Comienza
+            mostrándote cómo funciona la interfaz, qué opciones tienes al abrir
+            el programa y cómo encontrar plantillas para empezar. Luego, te
+            enseña a insertar y modificar elementos como textos, imágenes,
+            tablas, y hasta objetos 3D, para hacer tus presentaciones más
+            atractivas.
+          </p>
+        </div>
+        <div class="column">
+          <figure class="image video-container">
+            <iframe
+              class="has-ratio responsive-iframe"
+              width="640"
+              height="360"
+              src="https://www.youtube.com/embed/ra0hKJv8zbw"
+              frameborder="0"
+              allowfullscreen
+              style="min-width: 400px; max-width: 640"
+            ></iframe>
+          </figure>
+          <p class="has-text-grey-light is-size-5">
+            Video sacado del canal de Youtube:
+            <a href="https://www.youtube.com/@gallardoyoney" target="_blank">
+              <span class="icon has-text-danger">
+                <i class="bi bi-youtube"></i>
+              </span>
+              <span>Yoney Gallardo</span>
+            </a>
+          </p>
+          <a href="https://www.youtube.com/watch?v=ra0hKJv8zbw" target="_blank">
+            <span class="icon has-text-danger">
+              <i class="bi bi-link-45deg"></i>
+            </span>
+            <span>Link Video</span>
+          </a>
+        </div>
+      </div>
+
+      <br />
+    </section>`,
+  "/pages/powerpoint#documentos": `<section class="section">
+      <article class="panel is-info">
+        <p class="panel-heading has-text-centered">Documentos</p>
+        <p class="panel-tabs">
+          <a class="is-active">PDF</a>
+        </p>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            manual de powerpoint
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/powerpoint/manual-de-power-point.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://orenatocaunp.wordpress.com/wp-content/uploads/2018/06/manual-de-power-point.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            Microsoft PowerPoint Avanzado 2013
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/powerpoint/Microsoft PowerPoint Avanzado 2013.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://www.saludarequipa.gob.pe/camana/curso1/modulos/Microsoft%20PowerPoint%20Avanzado%202013.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            manual avanzado powerpoint 2016 ricosoft
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/powerpoint/manual-avanzado-powerpoint-2016-ricosoft.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://lapaucompetic.wordpress.com/wp-content/uploads/2014/09/manual-avanzado-powerpoint-2016-ricosoft.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>`,
+};
+
+// Función para navegar a una ruta específica
+function navigateTo(event, path) {
+  event.preventDefault();
+  window.history.pushState({}, path, window.location.origin + path);
+  renderContent(path);
+}
+
+// Función para cargar el contenido basado en la ruta actual
+function renderContent(path) {
+  const appDiv = document.getElementById("secciones");
+  appDiv.innerHTML =
+    routes[path] ||
+    `<section class="section">
+          <h1 class="title has-text-centered">Informacion</h1>
+    
+          <div
+            class="section is-flex is-flex-wrap-wrap is-justify-content-space-evenly"
+          >
+            <div class="section" style="max-width: 500px">
+              <h1 class="title">¿Para qué sirve PowerPoint?</h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Crear presentaciones visuales: </strong>Es ideal para diseñar presentaciones para reuniones, conferencias, clases y eventos.
+                  </li>
+                  <li>
+                    <strong>Visualización de ideas: </strong>Ayuda a presentar conceptos de forma clara y atractiva mediante el uso de diapositivas visualmente organizadas.
+  
+                  </li>
+                  <li>
+                    <strong>Mostrar contenido multimedia: </strong>Permite insertar imágenes, videos, audios, y gráficos interactivos que hacen la presentación más dinámica y entretenida.
+                  </li>
+                  <li>
+                    <strong>Diseño personalizado: </strong>Ofrece herramientas para personalizar el diseño de las diapositivas con plantillas, transiciones, animaciones y efectos.
+                  </li>
+                  <li>
+                    <strong>Impresión de diapositivas: </strong>Permite imprimir presentaciones en formato de diapositivas o en formato de notas, lo que facilita la distribución de los contenidos.
+                  </li>
+                </ul>
+              </div>
+            </div>
+    
+            <div class="section" style="max-width: 500px">
+              <h1 class="title is-size-4">
+                ¿Cuáles son sus características principales?
+              </h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Diapositivas: </strong>Las presentaciones se estructuran en una secuencia de diapositivas que pueden incluir texto, imágenes, gráficos y otros elementos visuales.
+                  </li>
+                  <li>
+                    <strong>Plantillas de diseño: </strong>PowerPoint ofrece una variedad de plantillas prediseñadas para facilitar la creación de presentaciones profesionales.
+                  </li>
+                  <li>
+                    <strong>Animaciones y transiciones: </strong>Puedes añadir efectos visuales a las diapositivas, como animaciones en el texto o elementos gráficos, y transiciones entre diapositivas para que la presentación sea más atractiva.
+                  </li>
+                  <li>
+                    <strong>Herramientas de colaboración: </strong>Permite que varias personas trabajen en la misma presentación al mismo tiempo, y ofrece herramientas de comentarios y sugerencias.
+                  </li>
+                  <li>
+                    <strong>Integración multimedia: </strong>Puedes insertar y editar videos, sonidos, gráficos y enlaces interactivos, lo que hace las presentaciones más dinámicas.
+                  </li>
+                  <li>
+                    <strong>Modo de presentación: </strong>PowerPoint incluye un modo especial de presentación para mostrar las diapositivas en pantalla completa, con la opción de mostrar notas del presentador, controlar el avance de las diapositivas y usar un puntero láser.
+                  </li>
+                  <li>
+                    <strong>Gráficos y diagramas: </strong>Puedes insertar gráficos, tablas y diagramas para ilustrar mejor los datos y conceptos durante la presentación.
+                  </li>
+                </ul>
+              </div>
+            </div>
+    
+            <div class="section" style="max-width: 500px">
+              <h1 class="title">¿Quiénes lo usan?</h1>
+              <div class="content">
+                <ul>
+                  <li>
+                    <strong>Profesionales: </strong>Para presentar propuestas, informes, proyectos, o informes de ventas a colegas, clientes o directivos.
+                  </li>
+                  <li>
+                    <strong>Estudiantes y educadores: </strong>Para crear y mostrar presentaciones en clase, ya sea para enseñar o presentar trabajos y proyectos.
+                  </li>
+                  <li>
+                    <strong>Empresas: </strong>Para realizar presentaciones en reuniones de negocios, conferencias y seminarios.
+  
+                  </li>
+                  <li>
+                    <strong>Marketers y creativos: </strong>Para mostrar conceptos de diseño, estrategias de marketing o informes de resultados.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>`; // Renderiza el contenido o un 404 si no existe la ruta
+}
+
+// Escucha los cambios en el historial para manejar la navegación del navegador (botones de atrás y adelante)
+window.onpopstate = () => {
+  renderContent(window.location.pathname); // Llama a renderContent con la ruta actual
+};
+
+// Carga la ruta inicial al cargar la página
+window.onload = () => {
+  renderContent(window.location.pathname); // Muestra el contenido inicial al cargar
+};
