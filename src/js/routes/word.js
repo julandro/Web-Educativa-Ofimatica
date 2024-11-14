7eac305c465251e6a34cc49144e4249353787a93
Julian Camacho <julandro.mza@gmail.com>
2024-11-14 00:19:25 -0500

    Se crea la sección de Word
    Se crea la sección de word, también se arreglan algunos problemas de redireccionamiento a las secciones por el enrutamiento

---
+++ src/js/routes/word.js
@@ -0,0 +1,467 @@
+const routes = {
+  "/pages/word#informacion": `
+      <section class="section">
+        <h1 class="title has-text-centered">Informacion</h1>
+  
+        <div
+          class="section is-flex is-flex-wrap-wrap is-justify-content-space-evenly"
+        >
+          <div class="section" style="max-width: 500px">
+            <h1 class="title">¿Para qué sirve Word?</h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Creación de documentos: </strong>Es ideal para escribir todo tipo de textos, desde cartas, informes y ensayos hasta documentos académicos y profesionales.
+                </li>
+                <li>
+                  <strong>Formato de texto: </strong>Ofrece una amplia variedad de herramientas para dar formato a los textos, como cambiar fuentes, tamaños, colores, alineación, y mucho más.
+
+                </li>
+                <li>
+                  <strong>Inserción de elementos multimedia: </strong>Permite agregar imágenes, gráficos, tablas, formas, y otros elementos visuales para enriquecer el documento.
+                </li>
+                <li>
+                  <strong>Edición y corrección: </strong>Incluye herramientas de corrección ortográfica y gramatical, que ayudan a mejorar la precisión del texto.
+                </li>
+              </ul>
+            </div>
+          </div>
+  
+          <div class="section" style="max-width: 500px">
+            <h1 class="title is-size-4">
+              ¿Cuáles son sus características principales?
+            </h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Interfaz intuitiva: </strong>La interfaz de Word está diseñada para ser fácil de usar y cuenta con una barra de herramientas que facilita el acceso a sus funciones principales.
+                </li>
+                <li>
+                  <strong>Estilos de texto: </strong>Permite crear y aplicar estilos de formato a párrafos, títulos y subtítulos para una apariencia uniforme.
+
+                </li>
+                <li>
+                  <strong>Tablas y gráficos: </strong>Word incluye herramientas para insertar y dar formato a tablas y gráficos, lo que ayuda en la organización de información visual.
+                </li>
+                <li>
+                  <strong>Revisión y comentarios: </strong>Facilita la revisión del documento con herramientas para agregar comentarios, realizar un seguimiento de cambios y aceptar o rechazar modificaciones.
+                </li>
+                <li>
+                  <strong>Opciones de diseño: </strong>Permite agregar encabezados, pies de página, números de página y dividir el documento en secciones para un diseño más profesional.
+                </li>
+                <li>
+                  <strong>Exportación y compatibilidad: </strong>Permite guardar y exportar documentos en múltiples formatos, como PDF y HTML, además de compartir fácilmente.
+                </li>
+              </ul>
+            </div>
+          </div>
+  
+          <div class="section" style="max-width: 500px">
+            <h1 class="title">¿Quiénes lo usan?</h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Estudiantes y académicos: </strong>Para elaborar ensayos, investigaciones, y otros trabajos escritos.
+                </li>
+                <li>
+                  <strong>Profesionales: </strong>Para redactar informes, memorándums, propuestas, y otros documentos de trabajo.
+                </li>
+                <li>
+                  <strong>Escritores: </strong>Para crear textos literarios, como cuentos, novelas, artículos, etc.
+
+                </li>
+                <li>
+                  <strong>Empleados de oficina: </strong>Para tareas diarias de comunicación y documentación.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      `,
+  "/pages/word#videos": `<section class="section">
+      <h1 class="title">Videos</h1>
+      <h2 class="subtitle">
+        Estos videos explicativos <strong>no son de nuestra autoria</strong>, al
+        lado esta el nombre del autor y su respectivo link.
+      </h2>
+
+      <br />
+
+      <div class="columns notification">
+        <div class="column content">
+          <h1 class="title">⏺ CURSO COMPLETO DE WORD 2023 - El Tío Tech</h1>
+          <p class="has-text-grey-light is-size-5 has-text-justified">
+            guía completa para dominar Microsoft Word, desde las herramientas
+            básicas hasta funciones avanzadas. Inicia con una introducción a la
+            interfaz y explica cómo personalizar el espacio de trabajo. A lo
+            largo del curso, se cubren temas clave como el formato y estilo de
+            texto, la organización de documentos mediante títulos y tablas de
+            contenido, y la inserción de elementos multimedia como imágenes y
+            gráficos. También se enseñan herramientas para crear tablas,
+            gráficos, formularios y plantillas personalizadas, así como opciones
+            de autoguardado y recuperación. Es un recurso completo para aprender
+            a manejar Word con eficiencia y profesionalismo.
+          </p>
+        </div>
+        <div class="column">
+          <figure class="image video-container">
+            <iframe
+              class="has-ratio responsive-iframe"
+              width="640"
+              height="360"
+              src="https://www.youtube.com/embed/n0yn3TZ1lSQ"
+              frameborder="0"
+              allowfullscreen
+              style="min-width: 400px; max-width: 640"
+            ></iframe>
+          </figure>
+          <p class="has-text-grey-light is-size-5">
+            Video sacado del canal de Youtube:
+            <a href="https://www.youtube.com/c/ElT%C3%ADoTech" target="_blank">
+              <span class="icon has-text-danger">
+                <i class="bi bi-youtube"></i>
+              </span>
+              <span> El Tío Tech</span>
+            </a>
+          </p>
+          <a href="https://www.youtube.com/watch?v=n0yn3TZ1lSQ" target="_blank">
+            <span class="icon has-text-danger">
+              <i class="bi bi-link-45deg"></i>
+            </span>
+            <span>Link Video</span>
+          </a>
+        </div>
+      </div>
+
+      <br />
+
+      <div class="columns notification">
+        <div class="column">
+          <figure class="image video-container">
+            <iframe
+              class="has-ratio responsive-iframe"
+              width="640"
+              height="360"
+              src="https://www.youtube.com/embed/2jvEaBEJinI"
+              frameborder="0"
+              allowfullscreen
+              style="min-width: 400px; max-width: 640"
+            ></iframe>
+          </figure>
+          <p class="has-text-grey-light is-size-5">
+            Video sacado del canal de Youtube:
+            <a href="https://www.youtube.com/@DostinHurtado" target="_blank">
+              <span class="icon has-text-danger">
+                <i class="bi bi-youtube"></i>
+              </span>
+              <span>Dostin Hurtado</span>
+            </a>
+          </p>
+          <a href="https://www.youtube.com/watch?v=2jvEaBEJinI" target="_blank">
+            <span class="icon has-text-danger">
+              <i class="bi bi-link-45deg"></i>
+            </span>
+            <span>Link Video</span>
+          </a>
+        </div>
+        <div class="column content">
+          <h1 class="title">
+            Curso Completo de Word 2024 en 1 Vídeo - Básico e Intermedio 🎓
+            Dostin Hurtado
+          </h1>
+          <p class="has-text-grey-light is-size-5 has-text-justified">
+            curso completo de Microsoft Word 2024, enseñando desde lo básico
+            hasta conceptos intermedios. Cubre temas como la interfaz de
+            usuario, guardado de documentos, formato de texto y párrafo,
+            inserción de imágenes y tablas, uso de encabezados y pies de página,
+            y cómo utilizar plantillas y combinar correspondencia. También da
+            recomendaciones para obtener una licencia de Office y optimizar el
+            rendimiento en computadoras. Este curso brinda los conocimientos
+            esenciales para usar Word de manera eficiente en tareas cotidianas y
+            profesionales.
+          </p>
+        </div>
+      </div>
+
+      <br />
+      
+    </section>`,
+  "/pages/word#documentos": `<section class="section">
+      <article class="panel is-info">
+        <p class="panel-heading has-text-centered">Documentos</p>
+        <p class="panel-tabs">
+          <a class="is-active">PDF</a>
+        </p>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            Word para universitarios. Tutorial para la confección de documentos
+            académicos
+
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/Word para universitarios. Tutorial para la confección de documentos académicos.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://dehesa.unex.es:8443/bitstream/10662/11684/6/978-84-09-25217-6.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            Word - Fundamentals Manual
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/Word-Fundamentals-Manual.pdf EXPERTO MICROSOFT EXCEL 2013.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://www.sgul.ac.uk/about/our-professional-services/information-services/library/documents/training-manuals/Word-Fundamentals-Manual.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            manual microsoft office word 2010.pdf
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/manual-microsoft-office-word-2010.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://tchefonsecalfaro.wordpress.com/wp-content/uploads/2011/07/manual-microsoft-office-word-2010.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            WORD 2013
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/WORD 2013.compressed.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://www.saludarequipa.gob.pe/camana/curso1/WORD%202013/WORD%202013.compressed.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            manual de word 2016
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/manual-de-word-2016.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+
+            <a
+              href="https://iepctabasco.mx/docs/capacitacion/manuales/manuales-diversos/manual-de-word-2016.pdf"
+              target="_blank"
+            >
+              <button class="button is-dark">
+                <i class="bi bi-box-arrow-up"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div class="panel-block">
+          <div class="column">
+            <span class="panel-icon">
+              <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            Manual de Microsoft Word 2016
+            <span class="tag is-dark">PDF</span>
+          </div>
+          <div>
+            <a
+              href="../../src/assets/docs/word/Manual de Microsoft Word 2016.pdf"
+              download
+            >
+              <button class="button is-success">
+                <i class="bi bi-download"></i>
+              </button>
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>`,
+};
+
+// Función para navegar a una ruta específica
+function navigateTo(event, path) {
+  event.preventDefault();
+  window.history.pushState({}, path, window.location.origin + path);
+  renderContent(path);
+}
+
+// Función para cargar el contenido basado en la ruta actual
+function renderContent(path) {
+  const appDiv = document.getElementById("secciones");
+  appDiv.innerHTML =
+    routes[path] ||
+    `<section class="section">
+        <h1 class="title has-text-centered">Informacion</h1>
+  
+        <div
+          class="section is-flex is-flex-wrap-wrap is-justify-content-space-evenly"
+        >
+          <div class="section" style="max-width: 500px">
+            <h1 class="title">¿Para qué sirve Word?</h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Creación de documentos: </strong>Es ideal para escribir todo tipo de textos, desde cartas, informes y ensayos hasta documentos académicos y profesionales.
+                </li>
+                <li>
+                  <strong>Formato de texto: </strong>Ofrece una amplia variedad de herramientas para dar formato a los textos, como cambiar fuentes, tamaños, colores, alineación, y mucho más.
+
+                </li>
+                <li>
+                  <strong>Inserción de elementos multimedia: </strong>Permite agregar imágenes, gráficos, tablas, formas, y otros elementos visuales para enriquecer el documento.
+                </li>
+                <li>
+                  <strong>Edición y corrección: </strong>Incluye herramientas de corrección ortográfica y gramatical, que ayudan a mejorar la precisión del texto.
+                </li>
+              </ul>
+            </div>
+          </div>
+  
+          <div class="section" style="max-width: 500px">
+            <h1 class="title is-size-4">
+              ¿Cuáles son sus características principales?
+            </h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Interfaz intuitiva: </strong>La interfaz de Word está diseñada para ser fácil de usar y cuenta con una barra de herramientas que facilita el acceso a sus funciones principales.
+                </li>
+                <li>
+                  <strong>Estilos de texto: </strong>Permite crear y aplicar estilos de formato a párrafos, títulos y subtítulos para una apariencia uniforme.
+
+                </li>
+                <li>
+                  <strong>Tablas y gráficos: </strong>Word incluye herramientas para insertar y dar formato a tablas y gráficos, lo que ayuda en la organización de información visual.
+                </li>
+                <li>
+                  <strong>Revisión y comentarios: </strong>Facilita la revisión del documento con herramientas para agregar comentarios, realizar un seguimiento de cambios y aceptar o rechazar modificaciones.
+                </li>
+                <li>
+                  <strong>Opciones de diseño: </strong>Permite agregar encabezados, pies de página, números de página y dividir el documento en secciones para un diseño más profesional.
+                </li>
+                <li>
+                  <strong>Exportación y compatibilidad: </strong>Permite guardar y exportar documentos en múltiples formatos, como PDF y HTML, además de compartir fácilmente.
+                </li>
+              </ul>
+            </div>
+          </div>
+  
+          <div class="section" style="max-width: 500px">
+            <h1 class="title">¿Quiénes lo usan?</h1>
+            <div class="content">
+              <ul>
+                <li>
+                  <strong>Estudiantes y académicos: </strong>Para elaborar ensayos, investigaciones, y otros trabajos escritos.
+                </li>
+                <li>
+                  <strong>Profesionales: </strong>Para redactar informes, memorándums, propuestas, y otros documentos de trabajo.
+                </li>
+                <li>
+                  <strong>Escritores: </strong>Para crear textos literarios, como cuentos, novelas, artículos, etc.
+
+                </li>
+                <li>
+                  <strong>Empleados de oficina: </strong>Para tareas diarias de comunicación y documentación.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>`; // Renderiza el contenido o un 404 si no existe la ruta
+}
+
+// Escucha los cambios en el historial para manejar la navegación del navegador (botones de atrás y adelante)
+window.onpopstate = () => {
+  renderContent(window.location.pathname); // Llama a renderContent con la ruta actual
+};
+
+// Carga la ruta inicial al cargar la página
+window.onload = () => {
+  renderContent(window.location.pathname); // Muestra el contenido inicial al cargar
+};
